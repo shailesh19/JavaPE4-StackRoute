ca30ef6f1cefc87ffa001ceef8a306fca426e993
shailesh <beingshailesh19@gmail.com>
2019-07-23 10:30:21 +0530

    6th question added

---
+++ src/main/Java/com/stackroute/pe4/CheckName.java
@@ -0,0 +1,10 @@
+package com.stackroute.pe4;
+
+public class CheckName
+{
+    public boolean nameCheck(String inputString)
+    {
+        inputString.matches(".Harry.*");
+        return true;
+    }
+}
---
+++ src/test/Java/com/stackroute/pe4/CheckNameTest.java
@@ -0,0 +1,31 @@
+package com.stackroute.pe4;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CheckNameTest
+{
+    CheckName object;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        this.object = new CheckName();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void givenInputStringShouldCheckForNameAndReturnPositiveResult()
+    {
+        boolean actualResult = object.nameCheck("This is Harry");
+        boolean expectedResult = true;
+        assertEquals(expectedResult,actualResult);
+
+    }
+}
